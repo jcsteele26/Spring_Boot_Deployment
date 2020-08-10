6dbfb268ae266c36f3e8b86f79e98ff05800db07
Andre <jcsteele26@gmail.com>
2020-08-10 08:46:41 -0400

    Start code for Heroku

---
+++ springdeployment/src/main/java/com/steele/springdeployment/MainController.java
@@ -0,0 +1,14 @@
+package com.steele.springdeployment;
+
+import org.springframework.web.bind.annotation.GetMapping;
+ 
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController 
+public class MainController{
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello";
+    }
+}
